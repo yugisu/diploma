0eb926f10b718973ff16772561277e4dd0db1cd7
Dmytrii Puzyr <slymahel@gmail.com>
2021-04-23 20:44:10 +0300

    Implement basic graphql server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,69 @@
 import Koa from 'koa'
+import koaBody from 'koa-bodyparser'
 import { promisify } from 'util'
 import type { Server } from 'http'
 import { PrismaClient } from '@prisma/client'
+import { gql, ApolloServer, makeExecutableSchema } from 'apollo-server-koa'
 
-import { getGreeting } from 'utils/greeting'
+import { __DEV__ } from 'enums/constants'
 
 const prisma = new PrismaClient()
 
-export const shutdownServer = (server: Server) => {
-  promisify(server.close)()
+const typeDefs = gql`
+  type User {
+    id: Int
+    email: String
+  }
 
-  prisma.$disconnect()
+  type Query {
+    user: User
+    number: Float
+  }
+`
+
+const resolvers = {
+  Query: {
+    user: async () => ({ id: 1, email: 'email' }),
+
+    number: async () => 2,
+  },
 }
 
-export const initServer = async () => {
-  const app = new Koa()
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+  resolverValidationOptions: {
+    requireResolversForResolveType: false,
+  },
+  inheritResolversFromInterfaces: true,
+})
 
+const apolloServer = new ApolloServer({
+  schema,
+  context: (ctx) => ({ ...ctx, prisma }),
+  formatError: (error) => (console.log(error), error),
+})
+
+export const initServer = async () => {
   await prisma.$connect()
 
-  app.use(async (ctx) => {
-    ctx.body = getGreeting()
-  })
+  const app = new Koa()
+
+  app.use(koaBody())
+
+  await apolloServer.start()
+
+  apolloServer.applyMiddleware({ app })
 
   const server = app.listen({ port: 3000 }, () => {
     console.log('Server listening on localhost:3000...')
   })
 
   return server
+}
+
+export const shutdownServer = (server: Server) => {
+  promisify(server.close)()
+
+  prisma.$disconnect()
 }
