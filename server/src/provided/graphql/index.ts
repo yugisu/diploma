57bd38e73966687aae24c4d3b9a5a983fe865a2a
Dmytrii Puzyr <slymahel@gmail.com>
2021-04-25 23:30:51 +0300

    Disable entity-level validations in type-graphql

--- server/src/provided/graphql/index.ts
+++ server/src/provided/graphql/index.ts
@@ -3,7 +3,7 @@ import { buildSchema } from 'type-graphql'
 import { UserCrudResolver } from 'generated/typegraphql-prisma/resolvers/crud'
 
 export const getGqlSchema = async () => {
-  const graphqlSchema = await buildSchema({ resolvers: [UserCrudResolver] })
+  const graphqlSchema = await buildSchema({ resolvers: [UserCrudResolver], validate: false })
 
   return graphqlSchema
 }
