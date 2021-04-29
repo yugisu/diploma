f82f04e264cdc3d02c5211aa0a6b4be335786248
Dmytrii Puzyr <slymahel@gmail.com>
2021-04-29 21:01:14 +0300

    Implement basic custom button component

---
+++ frontend/src/components/Common/Button.tsx
@@ -0,0 +1,54 @@
+import { lighten, transparentize } from 'polished'
+import React from 'react'
+import styled, { css } from 'styled-components'
+import { space, SpaceProps, layout, LayoutProps } from 'styled-system'
+
+type Props = {
+  primary?: boolean
+  outline?: boolean
+  linkLike?: boolean
+} & SpaceProps &
+  LayoutProps &
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button = (props: Props) => {
+  return <Inner {...props} />
+}
+
+const Inner = styled.button<{
+  primary?: boolean
+  outline?: boolean
+  linkLike?: boolean
+}>`
+  min-height: 1em;
+
+  padding: 0.4rem 0.9rem;
+
+  font-weight: bold;
+
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 0.25rem;
+
+  ${(props) => css`
+    color: ${lighten(0.2, props.theme.colors.text)};
+    background-color: ${transparentize(0.7, props.theme.colors.gray10)};
+    border-color: ${transparentize(0.6, props.theme.colors.gray10)};
+  `}
+
+  ${(props) =>
+    props.primary &&
+    css`
+      color: ${props.theme.colors.white};
+      background-color: ${props.theme.colors.orange13};
+      border-color: ${transparentize(0.7, props.theme.colors.primary)};
+    `}
+
+  &:focus {
+    outline: none;
+    border-color: ${(props) => props.theme.colors.primary};
+  }
+
+  ${space}
+  ${layout}
+`
