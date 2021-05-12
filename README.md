10138d837b2620998138c81c6168e95ee8f8431a
Dmytrii Puzyr <slymahel@gmail.com>
2021-05-12 22:46:25 +0300

    Update development setup

--- README.md
+++ README.md
@@ -8,3 +8,14 @@
 ├── server   - Application's backend written in TypeScript Koa
 └── shared   - Library of shared utilities, validators, types etc.
 ```
+
+## Development
+
+### Prerequisites
+
+You need Node.js v14 and Yarn v1 installed on your local machine.
+
+### Running project
+
+To run the project for development purposes, use the `run-development.sh` script by running it as a script (before usage, run `$ chmod +x run-development.sh`):
+`$ ./run-development.sh`
---
+++ run-development.sh
@@ -0,0 +1,17 @@
+START_DELAY_SERVER=4
+START_DELAY_FRONTEND=11
+
+trap "docker-compose down" INT
+
+docker-compose up -d database
+
+# Run shared
+(cd shared && yarn watch | sed "s/^/`printf '\033[36m(shared)\033[0m'`   /;") &
+
+# Run server
+(sleep $START_DELAY_SERVER && cd server && yarn start | sed "s/^/`printf '\033[95m(server)\033[0m'`   /;") &
+
+# Run frontend
+(sleep $START_DELAY_FRONTEND && cd frontend && yarn start | sed "s/^/`printf '\033[94m(frontend)\033[0m'` /;") &
+
+wait
