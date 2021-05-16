736e642fc583d097fceec9111f3aa76f3f45a67a
Dmytrii Puzyr <slymahel@gmail.com>
2021-05-16 22:58:01 +0300

    Update authVar default value

--- frontend/src/vars/authVar.ts
+++ frontend/src/vars/authVar.ts
@@ -1,3 +1,3 @@
 import { makeVar } from '@apollo/client'
 
-export const authVar = makeVar<boolean | undefined>(Boolean(localStorage.getItem('identity')))
+export const authVar = makeVar<boolean | undefined>(localStorage.getItem('identity') ? undefined : false)
