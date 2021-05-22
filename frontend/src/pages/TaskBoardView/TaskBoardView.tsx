632055c3b89e814e309119387ad3d6e8e8428fa4
Dmytrii Puzyr <slymahel@gmail.com>
2021-05-22 21:23:32 +0300

    Update status column labels

--- frontend/src/pages/TaskBoardView/TaskBoardView.tsx
+++ frontend/src/pages/TaskBoardView/TaskBoardView.tsx
@@ -7,7 +7,14 @@ import { TaskStatus } from 'generated/graphql-types'
 
 import * as Gql from './TaskBoardView.graphql.module'
 
-const statusesArray: TaskStatus[] = ['TODO', 'IN_PROGRESS', 'REVIEW', 'QA', 'DONE']
+// TODO: Remove this map and make columns configurable
+const statusMap: Record<Exclude<TaskStatus, 'INFO'>, string> = {
+  TODO: 'To do',
+  IN_PROGRESS: 'In progress',
+  REVIEW: 'Review',
+  QA: 'QA',
+  DONE: 'Done',
+}
 
 export const TaskBoardView = () => {
   const tasksQuery = useQuery(Gql.GetTasksDocument)
@@ -24,7 +31,7 @@ export const TaskBoardView = () => {
 
     if (draggableId && destination) {
       const taskExists = tasks.some((t) => t.id === draggableId)
-      const newStatus = statusesArray.find((s) => s === destination.droppableId)
+      const newStatus = destination.droppableId as keyof typeof statusMap
 
       if (taskExists && newStatus && newStatus !== source.droppableId) {
         execUpdateTaskStatus({
@@ -48,9 +55,9 @@ export const TaskBoardView = () => {
     <div className="h-full flex flex-col">
       <div className="flex-shrink-0 h-8 py-1 px-4 flex justify-between bg-gray-500 bg-opacity-5 shadow-sm" />
 
-      <div className="flex-1 h-full overflow-auto pb-4 grid grid-cols-5">
+      <div className="flex-1 h-full overflow-auto pb-4 px-4 grid grid-cols-5 gap-4">
         <DragDropContext onDragEnd={onTaskDragEnd}>
-          {statusesArray.map((status) => {
+          {Object.entries(statusMap).map(([status, statusTitle]) => {
             const relatedTasks = tasks.filter((task) => task.status === status)
 
             return (
@@ -58,7 +65,7 @@ export const TaskBoardView = () => {
                 {(droppableProvided, droppableSnapshot) => (
                   <div
                     className={clsx(
-                      'relative mt-4 mx-2 flex flex-col rounded bg-gray-500',
+                      'flex-1 relative mt-4 flex flex-col rounded bg-gray-500',
                       droppableSnapshot.isDraggingOver ? 'bg-opacity-10' : 'bg-opacity-5',
                     )}
                   >
@@ -66,7 +73,7 @@ export const TaskBoardView = () => {
                       style={{ maxWidth: '19rem' }}
                       className="sticky top-2 ml-2 mt-2 px-3 py-1 w-min truncate font-bold text-sm rounded shadow bg-gray-100 dark:bg-gray-900"
                     >
-                      {status}
+                      {statusTitle}
                     </div>
 
                     <div className="absolute top-3.5 right-5 text-sm font-bold opacity-50">{relatedTasks.length}</div>
@@ -84,7 +91,7 @@ export const TaskBoardView = () => {
                               {...provided.draggableProps}
                               {...provided.dragHandleProps}
                               className={clsx(
-                                'my-1 px-3 py-2 flex flex-col cursor-pointer rounded bg-gray-200 dark:bg-gray-800',
+                                'my-1 px-3 py-2 flex flex-col rounded bg-gray-200 dark:bg-gray-800',
                                 !snapshot.isDragging && 'shadow-sm',
                               )}
                             >
